623973561f7b4177a8ab7684d8f080f1229d7699
emount4 <domashev.eg@yandex.ru>
2025-09-07 14:03:07 +0300

    feat: program file ver 1.0

--- CS РТ 1.1/Program.cs
+++ CS РТ 1.1/Program.cs
@@ -1,70 +1,182 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
 
-float memory;
 
-void memory_plus(float num)
+class Calculator
 {
-    memory = num;
-    return;
-}
+    static float memory = 0;
 
-void memory_minus(){
-    memory = 0;
-    return;
-}
+    static void memoryPlus(float x)
+    {
+        memory += x;
+        Console.WriteLine($"{x} добавлено в память, текущее значение в памяти: {memory}");
+    }
 
-double calc_func(double first,  double second)
-{
-    Console.WriteLine("Напишите знак: (+,-,*,/,%,1/x,x^2, MR, M+, M-)");
-    string sign;
-    sign = Convert.ToString(Console.ReadLine());
-    if (sign == "+")
+    static void memoryMinus(float x)
     {
-        return first + second;
+        memory -= x;
+        Console.WriteLine($"{x} вычтено из памяти, текущее значение в памяти: {memory}");
     }
-    else if (sign == "-")
+
+    static float memoryRecall()
     {
-        return first - second;
+        Console.WriteLine($"Текущее значение в памяти: {memory}");
+        return memory;
     }
-    else if (sign == "*")
+
+    static float calc_func(float first, float second, string sign)
     {
-        return first * second;
-    }
-    else if (sign == "/")
-    {
-        if (second == 0)
+
+        if (sign == "+")
         {
+            return first + second;
+        }
+        else if (sign == "-")
+        {
+            return first - second;
+        }
+        else if (sign == "*")
+        {
+            return first * second;
+        }
+        else if (sign == "/")
+        {
+            if (second == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+                return float.NaN;
+            }
+            else
+            {
+                return first / second;
+            }
+        }
 
+        else if (sign == "%")
+        {
+            if (second == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+                return float.NaN;
+            }
+            else
+            {
+                return first % second;
+            }
+        }
+        else if (sign == "1/x")
+        {
+            if (first == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+                return float.NaN;
+            }
+            else
+            {
+                return 1 / first;
+            }
+        }
+
+        else if (sign == "x^2")
+        {
+            return first * first;
+        }
+
+        else if (sign == "sqrt")
+        {
+            if (first < 0)
+            {
+                Console.WriteLine("Корень из отрицательного числа!");
+                return float.NaN;
+            }
+            else
+            {
+                return (float)Math.Sqrt(first);
+            }
         }
         else
         {
-            return first / second;
+            Console.WriteLine("Неизвестная операция");
+            return float.NaN;
         }
     }
-    else if (sign == "^")
+    static void Main()
     {
-        return Math.Pow(first, second);
+
+        Console.WriteLine("Добро пожаловать в калькулятор!");
+        Console.WriteLine("Доступные операции:");
+        Console.WriteLine("+, -, *, /, %, 1/x, x^2");
+        Console.WriteLine("M+ (добавить в память), M- (вычесть из памяти), MR (прочитать память)");
+        Console.WriteLine("Введите 'exit' для выхода.");
+
+        string input;
+        float result = 0;
+
+        while (true)
+        {
+            Console.WriteLine("\nВведите команду или операцию: ");
+            input = Console.ReadLine();
+
+            if (input == null || input.ToLower() == "exit")
+            {
+                Console.WriteLine("Завершение работы");
+                break;
+            }
+
+            if (input == "MR")
+            {
+                result = memoryRecall();
+
+                continue;
+            }
+
+            if (input == "M+")
+            {
+                memoryPlus(result);
+            }
+            else if (input == "M-")
+            {
+                memoryMinus(result);
+            }
+
+            if ( input == "MR" || input == "M+" || input == "M-")
+            {
+                Console.WriteLine("\nВведите операцию: ");
+                input = Console.ReadLine();
+            }
+
+            Console.Write("Введите первое число: ");
+            if (!float.TryParse(Console.ReadLine(), out float first))
+            {
+                Console.WriteLine("Ошибка: введите корректное число.");
+                continue;
+            }
+
+            float second = 0;
+            if (!(input == "1/x" || input == "x^2" || input == "MR"))
+            {
+                Console.Write("Введите второе число: ");
+                if (!float.TryParse(Console.ReadLine(), out second))
+                {
+                    Console.WriteLine("Ошибка: введите корректное число.");
+                    continue;
+                }
+            }
+            result = calc_func(first, second, input);
+
+            if (float.IsInfinity(result))
+            {
+                Console.WriteLine("Переполнение! Результат слишком большой.");
+                return;
+            }
+
+            if (!float.IsNaN(result))
+            {
+                Console.WriteLine($"Результат: {result}");
+            }
+        }
     }
-    return 0;
 }
 
-double first, second;
-byte status;
-Console.WriteLine("1 - начать, 0 - завершить");//интерфейс
-status = Convert.ToByte(Console.ReadLine());
-switch (status){
-    case 1:
-        first = Convert.ToDouble(Console.ReadLine());
-        second = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine(calc_func(first, second));
-        break;
-    case 0:
-        return;
-    default:
-        return;
-}
+
+
+
